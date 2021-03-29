96d0b5eeb2c1e7c4606b31ec465c60a5c7eca754
yolocat <developer@yolocat.dev>
2021-03-29 11:58:57 +0200

    Initial Commit

---
+++ Unity Gummybear/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Unity Gummybear
+//
+//  Created by Valter Sporrenstrand on 2021-03-29.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
