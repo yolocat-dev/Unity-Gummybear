26df95ae41815e6e58461034249c06f5c9ce690a
yolocat <developer@yolocat.dev>
2021-03-30 13:02:58 +0200

    beta-0.2

--- Unity Gummybear/ViewController.swift
+++ Unity Gummybear/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: NSViewController {
     @IBOutlet weak var type: NSSegmentedCell!
     @IBOutlet weak var path: NSPathControl!
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -23,9 +22,43 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
-    @IBAction func typeChanged(_ sender: Any) {
+    
+    func shell(_ command: String) -> String {
+        let task = Process();
+        let pipe = Pipe();
+        
+        task.standardOutput = pipe;
+        task.standardError = pipe;
+        task.arguments = ["-c", command];
+        task.launchPath = "/bin/zsh";
+        task.launch();
+        
+        let data = pipe.fileHandleForReading.readDataToEndOfFile();
+        let output = String(data: data, encoding: .utf8)!;
+        
+        return output;
+    }
+    
+    @IBOutlet weak var versionPopUp: NSPopUpButton!
+    
+    @IBAction func run(_ sender: NSButton) {
+        let url = URL(fileURLWithPath: path.stringValue);
+        let hubUrl = url.relativeString.replacingOccurrences(of: "%2520", with: "\\ ").split(separator: ":");
+        let out = shell("\(hubUrl[1])/Contents/MacOS/Unity\\ Hub -- --headless editors -i");
+        let versions = out.split(separator: "\n");
+        
+        for i in 0..<versions.count {
+            if(i == 0 || i == 1){
+                versionPopUp.removeItem(at: 0);
+            } else if(i == (versions.count-1)){
+                versionPopUp.removeItem(at: versions.count-1)
+            }
+            let versionString = String(versions[i]).split(separator: ",")
+            versionPopUp.addItem(withTitle: String(versionString[0]).replacingOccurrences(of: " ", with: ""))
+        }
+    }
+    
+    @IBAction func typeChanged(_ sender: NSSegmentedControl) {
         switch(type.selectedSegment){
         case 0:
             path.isHidden = false;
